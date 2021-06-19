3a4b1d1bac8596e58602f4233e93481808d1ce10
krzyofreak <61771176+krzyofreak@users.noreply.github.com>
2021-06-19 13:50:51 +0530

    checkout

--- src/sample/MainWindowController.java
+++ src/sample/MainWindowController.java
@@ -17,9 +17,18 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
+    DBconnect mdc = new DBconnect();
+    Connection con = getConnection();
+    Statement stmt;
 
     ObservableList<String> ciTimeList = FXCollections.observableArrayList("AM","PM");
     ObservableList<String> pkgsList = FXCollections.observableArrayList("Package 1","Package 2","Package 3");
@@ -31,13 +40,72 @@ public class MainWindowController implements Initializable {
 
     private int ro1=0,ro2=0,ro3=0,ro4=0,ro5=0,ro6=0,ro7=0,ro8=0,ro9=0,ro10=0;
 
+    private Connection getConnection() {
+        Connection myConn = mdc.getMyConnection();
+        return myConn;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         navBtn1.setStyle("-fx-background-color: #a3b5d9");
         ciTime.setItems(ciTimeList);
         pkgs.setItems(pkgsList);
         timeSlot.setItems(timeSlotList);
+        reservedRoom();
         rsPane.toFront();
+    }
+
+    private void reservedRoom() {
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+
+        String myStatement;
+
+        myStatement="select * from bill,bill_reservation,reservation where BillID = Bill_id and Reservation_id = ReservationID and (Check_in_date <= \""+date.format(now)+"\" and (Check_out_date >= \""+date.format(now)+"\" or Check_out_date is NULL)) and (Check_in_time <=\""+time.format(now)+"\" and (Check_out_time >= \""+time.format(now)+"\" or Check_out_time is NULL))";
+
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(myStatement);
+
+            while(rs.next()){
+                if(rs.getInt("RoomNo") == 1){
+                    ro1=3;
+                    r1.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 2){
+                    ro2=3;
+                    r2.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 3){
+                    ro3=3;
+                    r3.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 4){
+                    ro4=3;
+                    r4.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 5){
+                    ro5=3;
+                    r5.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 6){
+                    ro6=3;
+                    r6.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 7){
+                    ro7=3;
+                    r7.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 8){
+                    ro8=3;
+                    r8.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 9){
+                    ro9=3;
+                    r9.setStyle("-fx-background-color: red");
+                }else if(rs.getInt("RoomNo") == 10){
+                    ro10=3;
+                    r10.setStyle("-fx-background-color: red");
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        System.out.println(myStatement);
     }
 
     public void NavBtn1OnAction(ActionEvent event){
@@ -75,13 +143,21 @@ public class MainWindowController implements Initializable {
 
     }
 
-    public void r1OnAction(ActionEvent event){
-        if(ro1 != 0){
-            r1.setStyle("-fx-background-color: green");
-            ro1 =0;
-        }else if(ro1 == 0){
-            r1.setStyle("-fx-background-color: #dede01");
-            ro1 =1;
+    public void r7OnAction(ActionEvent event) throws IOException {
+        if (ro7 == 1) {
+            r7.setStyle("-fx-background-color: green");
+            ro7 = 0;
+        }else if(ro7 == 0){
+            r7.setStyle("-fx-background-color: #dede01");
+            ro7 =1;
+        }else if(ro7== 3){
+            Parent root2 = FXMLLoader.load(getClass().getResource("CheckoutWindow.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Check Out");
+            stage.setScene(new Scene(root2));
+            stage.show();
         }
     }
+
+
 }
