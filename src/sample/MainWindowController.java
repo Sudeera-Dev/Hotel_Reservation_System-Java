05852593d4d06c825a7940474f32f0d190124c21
krzyofreak <61771176+krzyofreak@users.noreply.github.com>
2021-07-10 15:14:08 +0530

    gitiignore updated

--- src/sample/MainWindowController.java
+++ src/sample/MainWindowController.java
@@ -9,10 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -21,7 +18,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
@@ -30,13 +29,16 @@ public class MainWindowController implements Initializable {
     Connection con = getConnection();
     Statement stmt;
 
-    ObservableList<String> ciTimeList = FXCollections.observableArrayList("AM","PM");
+
     ObservableList<String> pkgsList = FXCollections.observableArrayList("Package 1","Package 2","Package 3");
     ObservableList<String> timeSlotList = FXCollections.observableArrayList("Day","Night");
 
     @FXML Button navBtn1,navBtn2,navBtn3,navBtn4,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10;
     @FXML Pane rsPane,rrPane,sPane,repPane;
-    @FXML ChoiceBox ciTime,pkgs,timeSlot;
+    @FXML ChoiceBox pkgs,timeSlot;
+    @FXML DatePicker ciDate;
+    @FXML TextField ciTime,ciName,ciId,ciAdd,ciVehino,ciTp;
+    @FXML Label ciSelroom,ciDprice;
 
     private int ro1=0,ro2=0,ro3=0,ro4=0,ro5=0,ro6=0,ro7=0,ro8=0,ro9=0,ro10=0;
 
@@ -48,7 +50,6 @@ public class MainWindowController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         navBtn1.setStyle("-fx-background-color: #a3b5d9");
-        ciTime.setItems(ciTimeList);
         pkgs.setItems(pkgsList);
         timeSlot.setItems(timeSlotList);
         reservedRoom();
@@ -63,6 +64,10 @@ public class MainWindowController implements Initializable {
         String myStatement;
 
         myStatement="select * from bill,bill_reservation,reservation where BillID = Bill_id and Reservation_id = ReservationID and (Check_in_date <= \""+date.format(now)+"\" and (Check_out_date >= \""+date.format(now)+"\" or Check_out_date is NULL)) and (Check_in_time <=\""+time.format(now)+"\" and (Check_out_time >= \""+time.format(now)+"\" or Check_out_time is NULL))";
+
+
+        ciDate.setValue(LocalDate.now());
+        ciTime.setText(time.format(now).toString());
 
         try {
             stmt = con.createStatement();
@@ -108,6 +113,18 @@ public class MainWindowController implements Initializable {
         System.out.println(myStatement);
     }
 
+    public void ciClearOnAction(ActionEvent event){
+        ciTime.setText("");
+        ciDate.setValue(null);
+        ciAdd.setText("");
+        ciVehino.setText("");
+        ciId.setText("");
+        ciTp.setText("");
+        ciName.setText("");
+        ciSelroom.setText("");
+
+    }
+
     public void NavBtn1OnAction(ActionEvent event){
         navBtn1.setStyle("-fx-background-color: #a3b5d9");
         navBtn2.setStyle("-fx-background-color: #0c447b");
@@ -148,9 +165,73 @@ public class MainWindowController implements Initializable {
             r7.setStyle("-fx-background-color: green");
             ro7 = 0;
         }else if(ro7 == 0){
+            r1.setStyle("-fx-background-color: green");
+            r2.setStyle("-fx-background-color: green");
+            r3.setStyle("-fx-background-color: green");
+            r4.setStyle("-fx-background-color: green");
+            r5.setStyle("-fx-background-color: green");
+            r6.setStyle("-fx-background-color: green");
+            r7.setStyle("-fx-background-color: green");
+            r8.setStyle("-fx-background-color: green");
+            r9.setStyle("-fx-background-color: green");
+            r10.setStyle("-fx-background-color: green");
+
+            ro1=0;
+            ro2=0;
+            ro3=0;
+            ro4=0;
+            ro5=0;
+            ro6=0;
+            ro7=0;
+            ro8=0;
+            ro9=0;
+            ro10=0;
+
+            ciDprice.setText("Rs. 1500.00 per day");
             r7.setStyle("-fx-background-color: #dede01");
             ro7 =1;
+            ciSelroom.setText("Room 7 - non/AC - Double Bed");
         }else if(ro7== 3){
+            Parent root2 = FXMLLoader.load(getClass().getResource("CheckoutWindow.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Check Out");
+            stage.setScene(new Scene(root2));
+            stage.show();
+        }
+    }
+
+    public void r1OnAction(ActionEvent event) throws IOException {
+        if (ro1 == 1) {
+            r1.setStyle("-fx-background-color: green");
+            ro1 = 0;
+        }else if(ro1 == 0){
+            r1.setStyle("-fx-background-color: green");
+            r2.setStyle("-fx-background-color: green");
+            r3.setStyle("-fx-background-color: green");
+            r4.setStyle("-fx-background-color: green");
+            r5.setStyle("-fx-background-color: green");
+            r6.setStyle("-fx-background-color: green");
+            r7.setStyle("-fx-background-color: green");
+            r8.setStyle("-fx-background-color: green");
+            r9.setStyle("-fx-background-color: green");
+            r10.setStyle("-fx-background-color: green");
+
+            ro1=0;
+            ro2=0;
+            ro3=0;
+            ro4=0;
+            ro5=0;
+            ro6=0;
+            ro7=0;
+            ro8=0;
+            ro9=0;
+            ro10=0;
+
+            ciDprice.setText("Rs. 3000.00 per day");
+            r1.setStyle("-fx-background-color: #dede01");
+            ro1 =1;
+            ciSelroom.setText("Room 1 - AC - Double Bed");
+        }else if(ro1== 3){
             Parent root2 = FXMLLoader.load(getClass().getResource("CheckoutWindow.fxml"));
             Stage stage = new Stage();
             stage.setTitle("Check Out");
