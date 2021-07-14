21224d8cd9f46ac6313ff554b729b7d09896ea04
krzyofreak <61771176+krzyofreak@users.noreply.github.com>
2021-07-14 13:01:58 +0530

    Event Cancel

--- src/sample/EventWindowController.java
+++ src/sample/EventWindowController.java
@@ -2,11 +2,17 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -81,6 +87,16 @@ public class EventWindowController  implements Initializable {
 
     public void evClearOnAction(ActionEvent event){
         evPay.setText("");
+    }
+
+    public void reCancelOnAction(ActionEvent event) throws IOException {
+        MainWindowController.process = 2;
+        Parent root3 = FXMLLoader.load(getClass().getResource("ConfirmationWindow.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Confirm");
+        stage.setScene(new Scene(root3));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
     }
 
 }
