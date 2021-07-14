0fbd7fce1951cb83e74243612e5e46d9363d3647
lakshithadilshan <77695508+lakshithadilshan@users.noreply.github.com>
2021-07-14 18:46:52 +0530

    Update CheckOutProcess.java

--- src/sample/CheckOutProcess.java
+++ src/sample/CheckOutProcess.java
@@ -10,9 +10,8 @@ public class CheckOutProcess {
     Connection con = getConnection();
     Statement stmt;
 
-    DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    private String coDate,coTime,ciDate,hh;
+    private String coDate,coTime,ciDate,CusName;
     private long days=0, total=0;
 
     CheckOutProcess(String coDate,String coTime){
@@ -81,6 +80,51 @@ public class CheckOutProcess {
     }
 
     public String checkout(){
+        int Rid = MainWindowController.reservationID;
+
+        String query = "update reservation set Check_out_date = '"+coDate+"', Check_out_time = '"+coTime+"' where ReservationID='"+Rid+"'";
+        try {
+            PreparedStatement pstn = con.prepareStatement(query);
+//            pstn.setDate(1, Date.valueOf(coDate));
+//            pstn.setTime(2, Time.valueOf(coTime));
+            pstn.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        int Bill = MainWindowController.billId;
+        String queryy = "update bill set Amount ='"+total+"' ";
+
+        try {
+            PreparedStatement pstn = con.prepareStatement(queryy);
+            pstn.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        String myStatement = "select * from bill where BillID= '"+Bill+"'";
+        try {
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(myStatement);
+            while (rs.next()){
+                CusName = rs.getString("CustomerID");
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+
+        String queryinsret = "insert into ledger (Description,paid,Date,CustomerID,BillID) values ('Reservation','"+total+"','"+coDate+"','"+CusName+"','"+Bill+"')";
+        try {
+            PreparedStatement pstn = con.prepareStatement(queryinsret);
+
+            pstn.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
 
         return "success";
     }
